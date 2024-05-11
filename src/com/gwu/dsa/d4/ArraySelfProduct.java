b6cc3644e034973795bc23f24fd41056bea49736
Udit kaushik <udit08oct@gmail.com>
2024-05-11 15:49:22 +0530

    Add Day 4 solutions

---
+++ src/com/gwu/dsa/d4/ArraySelfProduct.java
@@ -0,0 +1,70 @@
+package com.gwu.dsa.d4;
+
+import java.util.Arrays;
+
+class ArraySelfProductSolution {
+
+    // Time complexity : O(N) =>  where n = number of elements in the array
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        int[] output = new int[n];
+        Arrays.fill(output, 0);
+        int zeroIndexIfCountIsOne = -1;
+        int product = 1;
+        for(int i = 0 ; i < n ; i++) {
+            if(nums[i] == 0) {
+                count++;
+                zeroIndexIfCountIsOne = i;
+            } else {
+                product = product * nums[i];
+            }
+        }
+
+        if(count > 1) {
+            return output;
+        }
+
+        if(count == 1) {
+            output[zeroIndexIfCountIsOne] = product;
+            return output;
+        }
+
+        for(int i = 0 ; i < n ; i++) {
+            output[i] = product / nums[i];
+        }
+
+        return output;
+    }
+}
+
+public class ArraySelfProduct {
+    public static void main(String[] args) {
+        ArraySelfProductSolution obj = new ArraySelfProductSolution();
+        int[] nums = new int[]{1,2,3,4};
+        int[] output = obj.productExceptSelf(nums);
+
+        for(int i = 0 ; i < output.length ; i++) {
+            System.out.println(output[i]);
+        }
+
+        System.out.println("Second array");
+
+        nums = new int[]{-10,2,3,0,5};
+        output = obj.productExceptSelf(nums);
+
+        for(int i = 0 ; i < output.length ; i++) {
+            System.out.println(output[i]);
+        }
+
+        System.out.println("Third array");
+
+        nums = new int[]{-10,0,3,0,5};
+        output = obj.productExceptSelf(nums);
+
+        for(int i = 0 ; i < output.length ; i++) {
+            System.out.println(output[i]);
+        }
+
+    }
+}
---
+++ src/com/gwu/dsa/d4/MoveNegativeElements.java
@@ -0,0 +1,71 @@
+package com.gwu.dsa.d4;
+
+class MoveNegativeElementsSolution {
+
+    public void segregateElements(int ar[], int n) {
+        // Your code goes here
+
+        int posCount = 0;
+        int negCount = 0;
+
+
+        // T.C = O(N) = N
+        for(int i = 0 ; i < n ; i++) {
+            if(ar[i] >= 0) {
+                posCount++;
+            } else {
+                negCount++;
+            }
+        }
+
+
+
+
+        // Combined Space Complexity = O(N) => where N = posCount + negCount;
+        int pos[] = new int[posCount];
+        int posIndex = 0;
+
+
+        int neg[] = new int[n - posCount];
+        int negIndex = 0;
+
+
+
+        // T.C = O(N) = N
+        for(int i = 0 ; i < n ; i++) {
+            if(ar[i] >= 0) {
+                pos[posIndex++] = ar[i];
+            } else {
+                neg[negIndex++] = ar[i];
+            }
+        }
+
+        // System.out.println("Positive numbers : " + posIndex + " Negative number : " + negIndex);
+
+
+        // Combined Time Complexity = O(N) => where N = posCount + negCount; = N
+        for(int i = 0 ; i < posIndex ; i++) {
+            ar[i] = pos[i];
+        }
+
+        for(int i = 0 ; i < negIndex ; i++) {
+            ar[posIndex + i] = neg[i];
+        }
+
+    }
+}
+
+public class MoveNegativeElements {
+    public static void main(String[] args) {
+        MoveNegativeElementsSolution obj = new MoveNegativeElementsSolution();
+        int ar[] = new int[]{2,3,4,-4,-9,1,-77,23,0};
+        int n = ar.length;
+        obj.segregateElements(ar, 9);
+
+        System.out.println("After segregation ");
+
+        for(int i = 0 ; i < n ; i++) {
+            System.out.println(ar[i]);
+        }
+    }
+}
